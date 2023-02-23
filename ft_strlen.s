af35471da7f681e9b2bf5a844e20fbc8b68e9007
Wolran <38337827+Wolran@users.noreply.github.com>
2023-02-23 02:03:42 +0100

    add strlen

---
+++ ft_strlen.s
@@ -0,0 +1,28 @@
+section .text
+	global ft_strlen
+
+ft_strlen:
+	xor rax, rax 			 ;je remet tout les bit 1 de rax a 0 pour avoir: rax = 0d
+	;mov rax, 0d
+
+while:
+	cmp byte[rdi + rax], 0d	; je compare str a l'index rax avec \0
+	je end					; saute vers end si le comparatif est correcte
+	inc rax					; j'incrémente rax de 1
+	jmp while				; sinon je loop sur while
+
+end:
+	ret
+
+------------------------------------------------------------------------------------------------
+section .text
+	global ft_strlen
+
+ft_strlen:
+	mov rax, -1d 			; je remet tout les bit 1 de rax a 0 pour avoir: rax = 0d 
+
+while:
+	inc rax					; j'incrémente rax de 1
+	cmp byte[rdi + rax], 0d	; je compare str a l'index rax avec \0
+	jne while				; si le compare n'est pas bon je loop sur while
+	ret						; je return la fin de la fonction
