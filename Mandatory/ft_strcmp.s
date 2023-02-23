7a35d149b3994aa613fab2ff01ee708c08a40f4c
Wolran <38337827+Wolran@users.noreply.github.com>
2023-02-23 08:22:10 +0100

    commentaire update

--- Mandatory/ft_strcmp.s
+++ Mandatory/ft_strcmp.s
@@ -17,20 +17,20 @@ ft_strcmp:
     xor rax, rax						; Initialisation de rax à 0
 
 while:
-    mov bl, byte [rdi + rax]			; mov le carac de la 1er chaine dans bl
-    mov cl, byte [rsi + rax]			; mov le carac de la 2de chaine dans cl
+    mov bl, byte [rdi + rax]					; mov le carac de la 1er chaine dans bl
+    mov cl, byte [rsi + rax]					; mov le carac de la 2de chaine dans cl
     cmp bl, cl							; compare des deux caractères
-    jne end								; si les caractères ne sont pas égaux, saute a end
-	test bl, bl							; test si bl = \0 (si bl est = 0 en ascii)
-    jz end								; test le zeroflag, si présent saute a end
+    jne end							; si les caractères ne sont pas égaux, saute a end
+	test bl, bl						; test si bl = \0 (si bl est = 0 en ascii)
+    jz end							; test le zeroflag, si présent saute a end
     test cl, cl							; test si cl = \0 (si cl est = 0 en ascii)
-    jz end								; test le zeroflag, si présent saute a end
-    inc rax								; incrémente rax de 1
+    jz end							; test le zeroflag, si présent saute a end
+    inc rax							; incrémente rax de 1
     jmp while							; boucle sur while
 
 end:
 	mov rcx, rax						; met la valeur rax dans rcx
-	mov rax, byte[rdi + rcx]			; met la derniere valeur trouver de la 1er chaine dans rax
-	mov rbx, byte[rsi + rcx]			; met la derniere valeur trouver de la 2de chaine dans rbx
+	mov rax, byte[rdi + rcx]				; met la derniere valeur trouver de la 1er chaine dans rax
+	mov rbx, byte[rsi + rcx]				; met la derniere valeur trouver de la 2de chaine dans rbx
 	sub rax, rbx						; soustrait rax (1er chaine) avec rbx (2de chaine) et met le resultat dans rax
-    ret									; return rax
+    ret								; return rax
