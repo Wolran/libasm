d66edd633080d8d2d5dff5b8d7b3803bd2c9f921
Wolran <38337827+Wolran@users.noreply.github.com>
2023-02-23 08:22:28 +0100

    commentaire update

--- Mandatory/ft_strcpy.s
+++ Mandatory/ft_strcpy.s
@@ -17,13 +17,13 @@ ft_strcpy:
 	xor rax, rax						; Initialisation de rax à 0
 
 while:
-	mov cl, byte[rsi + rax]				; place le caractere de la 2de chaine dans cl
-	mov byte[rdi + rax], cl				; place cl dans la premierre chaine
-	test cl, cl							; test si cl = \0 (si cl est = 0 en ascii) 
-	jz end								; test le zeroflag, si présent saute a end
-	inc rax								; incrémente rax de 1
+	mov cl, byte[rsi + rax]					; place le caractere de la 2de chaine dans cl
+	mov byte[rdi + rax], cl					; place cl dans la premierre chaine
+	test cl, cl						; test si cl = \0 (si cl est = 0 en ascii) 
+	jz end							; test le zeroflag, si présent saute a end
+	inc rax							; incrémente rax de 1
     jmp while							; boucle sur while
 
 end:
 	mov rax, rdi						; place rdi (1er chaine) dans rax
-	ret									; return rax
+	ret							; return rax
