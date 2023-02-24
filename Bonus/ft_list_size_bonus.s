c1b47d640518fc3427a921d5083b3894016f3dff
Wolran <38337827+Wolran@users.noreply.github.com>
2023-02-24 01:32:16 +0100

    uptade com

--- Bonus/ft_list_size_bonus.s
+++ Bonus/ft_list_size_bonus.s
@@ -14,14 +14,14 @@ section .text
 	global ft_list_size_bonus
 
 ft_list_size_bonus:
-	xor rax rax
+	xor rax rax				; Initialisation de rax à 0
 
 while:
-	test rdi, rdi
-	jz end
-	inc rax
-	mov rdi, [rdi + 8]
-	jmp while
+	test rdi, rdi				; test si rdi est NULL
+	jz end					; si rdi est NULL jump vers end
+	inc rax					; incremente rax de 1
+	mov rdi, [rdi + 8]			; met rdi a l'adresse du prochain element de la chaine
+	jmp while				; jump vers while
 
 end:
-	ret
+	ret					; return rax
